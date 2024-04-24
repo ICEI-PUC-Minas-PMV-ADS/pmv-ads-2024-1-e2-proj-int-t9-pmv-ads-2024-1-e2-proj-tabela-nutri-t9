28b80bfebd82c88a8cdddb170d852ec6940549ed
AldrinTaylorPUC <abittencoiurt@sga.pucminas.br>
2024-04-23 22:03:01 -0300

    Modelo de Estrutura ASP.NET

---
+++ Codigo-fonte/Data/dbContext.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+public class MyDbContext : DbContext
+{
+    public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+    public DbSet<MyEntity> MyEntities { get; set; }
+}
---
+++ Codigo-fonte/Model/tabelaModel.cs
@@ -0,0 +1,9 @@
+namespace ExampleBackend
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
