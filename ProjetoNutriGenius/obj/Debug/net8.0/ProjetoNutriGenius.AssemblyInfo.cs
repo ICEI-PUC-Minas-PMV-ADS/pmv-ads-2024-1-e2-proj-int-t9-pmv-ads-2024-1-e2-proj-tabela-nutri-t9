f03058800d527c8d8dbc2235f71b3bd7d022d2da
Julia Berto <jbrosa@sga.pucminas.br>
2024-06-08 16:44:21 -0300

    tentando sincronizar

--- ProjetoNutriGenius/obj/Debug/net8.0/ProjetoNutriGenius.AssemblyInfo.cs
+++ ProjetoNutriGenius/obj/Debug/net8.0/ProjetoNutriGenius.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoNutriGenius")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97c9c1c2bd879167e2b092cf1191a29c58654b2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36cc7f2ca58930409f3724422626aaeadfe10b61")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoNutriGenius")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoNutriGenius")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
